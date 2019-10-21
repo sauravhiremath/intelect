1f78252c327f08b5b3a31f3c2fc2586a2d9fb98e
sauravhiremath <sauravhiremath@gmail.com>
2019-10-21 15:37:18 +0530

    update: many changes for good

--- routes/homeRoutes.ts
+++ routes/homeRoutes.ts
@@ -1,26 +1,75 @@
 import { Router } from "express";
 import session from "express-session";
 import util from "util";
+var path = require("path");
 const axios = require("axios");
-const dialogflow = require('dialogflow').v2beta1;
+const multer = require("multer");
+// const dialogflow = require("dialogflow").v2beta1;
+const fs = require("fs");
 const getYoutubeSubtitles = require("@joegesualdo/get-youtube-subtitles-node");
 const exec = util.promisify(require("child_process").exec);
+var storage = multer.diskStorage({
+	destination: "uploads/",
+	filename: function(req, file, cb) {
+		//req.body is empty...
+		//How could I get the new_file_name property sent from client here?
+		cb(null, file.originalname + ".pdf");
+	}
+});
+var upload = multer({ storage: storage });
+
+// Imports the Google Cloud client library
+const language = require("@google-cloud/language");
+
+// Creates a client
+const client = new language.LanguageServiceClient();
+
+// Imports the Google Cloud client library
+const { Assistant, AssistantLanguage } = require("nodejs-assistant");
+
+// Your credentials
+const credentials = JSON.parse(
+	fs.readFileSync(
+		"client_secret_111559290713-ul71nii1d9gddutpe91u2qijrorg7nsd.apps.googleusercontent.com.json"
+	)
+);
+
+// console.log(credentials)
 
 const router = Router();
 export default router;
 
 router.get("/transcript", async (req, res) => {
-	console.log("working");
+	// console.log("working");
 	const id = req.query.transcript_id;
-    const transcript = await get_transcript_py(id);
-    res.json({
-        success: true,
-        message: transcript
-    })
+	const transcript = await get_transcript_py(id);
+	res.json({
+		success: true,
+		message: transcript
+	});
+});
+
+router.post("/upload", upload.single("portion"), async (req, res) => {
+	const fileName = req.body.fileName;
+	if (req.file) {
+		const { stdout, stderr } = await exec("python scripts/topics.py", fileName);
+		console.log(stdout);
+		console.log(stderr);
+
+		res.json({
+			success: true,
+			message: stdout
+		});
+	} else {
+		res.json({
+			success: false,
+			message: "File not uploaded properly"
+		});
+	}
 });
 
 router.get("/topics", async (req, res) => {
-    // const fileName = req.query.fileName;
+	const fileName = req.query.fileName;
 	const { stdout, stderr } = await exec("python scripts/topics.py");
 	console.log(stdout);
 	console.log(stderr);
@@ -31,56 +80,76 @@ router.get("/topics", async (req, res) => {
 	});
 });
 
-router.get("/processMgmt", async (req, res) => {
-	const { stdout, stderr } = await exec("python scripts/processMgmt.py");
-	console.log(stdout);
-	console.log(stderr);
+router.get("/gassist", async (req, res) => {
+	console.log("G Assist starts");
 
-	res.json({
-		success: true,
-		message: stdout
-	});
-});
+	const userQuery = req.query.userQuery;
+	console.log(userQuery);
 
-router.get("/processMgmtBeta", async (req, res) => {
-	const burstTime = req.body.burstTime;
-	console.log(burstTime);
-	const { stdout, stderr } = await exec(
-		"python scripts/processMgmt.py",
-		burstTime
+	// Creates a client
+	const assistant = new Assistant(
+		/* required credentials */ {
+			type: "authorized_user",
+			client_id: credentials.web.client_id,
+			client_secret: credentials.web.client_secret
+		}
+		/* additional, optional options */
 	);
-	console.log(stdout);
-	console.log(stderr);
 
-	res.json({
-		success: true,
-		message: stdout
+	// Sends a text query to the assistant
+	assistant
+		.query(userQuery)
+		.then(response => {
+			// response contains all the fields returned by the assistant, such as the text and audio
+			res.json({
+				success: true,
+				message: response.text
+			});
+			console.log(`Response: ${response.text}`);
+			// response.audio is a Buffer containing the audio response by the assistant
+		})
+		.catch(err => {
+			console.error("ERROR: ", err);
+		});
+});
+
+router.get("/mainTopics", async (req, res) => {
+	const transcript =
+		"all have similar properties. And the reason why they all\nhave similar properties is, in most cases, they have the same number of valence electrons. Remember, valence electrons\nare the reactive electrons, the ones that might\ninteract with other things. And because elements with\nsimilar valence electrons will have similar reactivities,\nthey will form similar ions. Similar ions. And they will have similar roles. Similar roles in ionic compounds. Ionic compounds. Now, for the sake of this\nvideo, I'm gonna focus most on the extremes of the periodic table, the groups at the left and the right, because those are the closest\nto having a full outer shell, either by losing electrons\nor by gaining electrons. So just to remind ourselves, what does it mean to have\na full, full outer shell? Well, in general, people\nwill refer to the octet rule. For our second, third, fourth,\nfifth, and on and on shells, you're full when you have eight electrons. Eight electrons. The major exception to the\noctet rule is the first shell, where it is full with two electrons";
+	// var bodyFormData = new FormData();
+	// bodyFormData.set("document", transcript);
+
+	// axios({
+	// 	method: "post",
+	// 	url: "https://language.googleapis.com/v1beta2/documents:classifyText",
+	// 	data: bodyFormData,
+	// 	config: { headers: { "Content-Type": "multipart/form-data" } }
+	// })
+	//     .then(function(response) {
+	//         //handle success
+	//         console.log(response);
+	//     })
+	//     .catch(function(response) {
+	//         //handle error
+	//         console.log(response);
+	//     });
+	console.log(123);
+	const document = {
+		content: transcript,
+		type: "PLAIN_TEXT"
+	};
+
+	// Classifies text in the document
+	const [classification] = await client.classifyText({ document });
+	console.log("Categories:");
+	classification.categories.forEach(category => {
+		console.log(
+			`Name: ${category.name}, Confidence: ${category.confidence}`
+		);
 	});
 });
 
-router.post("/networkMgmt", async (req, res) => {
-	const { stdout, stderr } = await exec("python scripts/networkMgmt.py");
-	console.log(stdout);
-	console.log(stderr);
-
-	res.json({
-		success: true,
-		message: stdout
-	});
-});
-
-router.post("/ioMgmt", async (req, res) => {
-	const { stdout, stderr } = await exec("python scripts/ioMgmt.py");
-	console.log(stdout);
-	console.log(stderr);
-
-	res.json({
-		success: true,
-		message: stdout
-	});
-});
-
-async function get_transcript_py(id){
-	const {stdout,stderr} = await exec('python scripts/transcript.py '+ id);
+async function get_transcript_py(id) {
+	const { stdout, stderr } = await exec("python scripts/transcript.py " + id);
 	return stdout;
 }
--- scripts/topics.py
+++ scripts/topics.py
@@ -5,8 +5,8 @@ import urllib.request
 import PyPDF2
 import random
 
-# file_path = sys.args[0]
-pdf_file = open('uploads/CSE2005_Operating-Systems_ETH_1_AC39.pdf', 'rb')
+file_path = 'uploads/' + sys.args[0]
+pdf_file = open(file_path, 'rb')
 read_pdf = PyPDF2.PdfFileReader(pdf_file)
 number_of_pages = read_pdf.getNumPages()
 page = read_pdf.getPage(0)
@@ -22,7 +22,7 @@ subj_name =""
 for line in page_content1.split('\n'):
     if line != ' ':
         if any(i.isdigit() for i in line) == False and line != '\n':
-            match = line
+            subj_name = line
             break
 
 # main_topics.append(re.findall('^(.*?):', page_content2, re.MULTILINE))
@@ -42,20 +42,23 @@ vid_time = ""
 # print(vid_time)
 
 for i in sub_topics[3:]:
-    # print(i)
+    print('Done with' + i)
     query_string = urllib.parse.urlencode({"search_query" : i + 'in' + subj_name})
     # print(query_string)
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
     # print(html_content.read().decode())
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    vid_time = re.findall(r'<span class="video-time" aria-hidden="true">(.*)', html_content.read().decode())[0]
-    timing_raw = re.findall(r'<span class="video-time" aria-hidden="true">(.*)', html_content.read().decode())[0]
-    for x in timing_raw:
-        if x.isdigit() == True or x==":":
-            vid_time +=x
-        else:
-            break
-    sub_videos.append({"url":"http://www.youtube.com/watch?v=" + search_results[0], "duration": vid_time, "channel": channel_name)
+    try:
+        # vid_time = re.findall(r'<span class="video-time" aria-hidden="true">(.*)', html_content.read().decode())[0]
+        timing_raw = re.findall(r'<span class="video-time" aria-hidden="true">(.*)', html_content.read().decode())[0]
+        for x in timing_raw:
+            if x.isdigit() == True or x==":":
+                vid_time +=x
+            else:
+                break
+    except:
+        vid_time = '13:37';
+    sub_videos.append({"url":"http://www.youtube.com/watch?v=" + search_results[0], "duration": vid_time, "channel": channel_name})
 
 
 print(subj_name, main_topics, sub_topics, sub_videos)
