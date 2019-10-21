0239bbc3203c7124355c896ce7c2663353be84b9
sauravhiremath <sauravhiremath@gmail.com>
2019-10-21 15:53:46 +0530

    fix: cross origin 2

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import session from "express-session";
 import http from "http";
 import homeRoutes from "./routes/homeRoutes";
 
-
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
